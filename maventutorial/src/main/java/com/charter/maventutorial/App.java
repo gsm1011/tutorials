ac3c82b470db710ac1686025e9b5931812a212c2
Shumin Guo <gsmsteve@gmail.com>
2014-08-15 12:07:02 -0400

    Added the maven tutorial project.

---
+++ maventutorial/src/main/java/com/charter/maventutorial/App.java
@@ -0,0 +1,13 @@
+package com.charter.maventutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
