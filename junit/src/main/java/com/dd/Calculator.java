b473d96a5f582221c53f51c36bb9375f39bdfd9c
Shumin <gsmsteve@gmail.com>
2016-04-04 09:27:49 -0500

    Changed the name of the project.

--- junit/src/main/java/com/dd/Calculator.java
+++ junit/src/main/java/com/dd/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-  public int evaluate(String expression) {
+  public int sum(String expression) {
     int sum = 0;
     for (String summand: expression.split("\\+"))
       sum += Integer.valueOf(summand);
--- junit/src/test/java/com/dd/CalculatorTest.java
+++ junit/src/test/java/com/dd/CalculatorTest.java
@@ -5,7 +5,7 @@ public class CalculatorTest {
   @Test
   public void evaluatesExpression() {
     Calculator calculator = new Calculator();
-    int sum = calculator.evaluate("1+2+3");
-    assertEquals(6, sum);
+    int sum = calculator.sum("1+2+3");
+    assertEquals(60, sum);
   }
 }
