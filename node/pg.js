78e3ec818dbbf046370143bc87cdc19408164be3
Shumin <gsmsteve@gmail.com>
2016-05-06 09:49:51 -0500

    Updated file to include insert and update queries.

--- node/pg.js
+++ node/pg.js
@@ -35,34 +35,53 @@ client.connect();
 //////////////////////////
 var query = client.query('SELECT * FROM velocity_boundary;');
 query.on('row', function (row) {
-    console.log(row); 
+    //console.log(row); 
 });
 
 ///////////////////////////////////////
 // read data and load to pg database.//
 ///////////////////////////////////////
-var pfo = JSON.parse(fs.readFileSync('./test.json', 'utf8'));
-var features = pfo[0].features[0].properties;
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(process.argv[2])
+});
 
-// fields used to update in the database. 
-var id = pfo[0].features[0].id; 
-var name = features.Name;
-var field = features.Field;
-var pipelinefn = features.PipeLineFn;
-var tags = features.Tags;
-var type = features.Type;
-var userid = features.UserId;
+lineReader.on('line', function (line) {
+    var pfo = JSON.parse(line, 'utf8');
+    var features = pfo[0].features[0].properties;
 
-var query = client.query('UPDATE velocity_boundary SET name = ($1), field=($2), pipelinefn=($3),tags=($4),userid=($5),kafka_msg=($6) WHERE id=($7)', [name, field, pipelinefn, tags, userid, JSON.stringify(pfo), id]);
+    // fields used to update in the database. 
+    var id = pfo[0].features[0].id; 
+    var name = features.Name;
+    var field = features.Field;
+    var pipelinefn = features.PipeLineFn;
+    var tags = features.Tags;
+    var type = features.Type;
+    var userid = features.UserId;
+    console.log(id);
+
+    try {
+	query = client.query('UPDATE velocity_boundary SET name = ($1), field=($2), pipelinefn=($3),tags=($4),userid=($5),kafka_msg=($6) WHERE id=($7)', [name, field, pipelinefn, tags, userid, line, id]);	
+    } catch (e) {
+	// console.log('record does not exist, trying to insert......');
+	// console.log('inserting into the query....'); 
+	query = client.query({
+	    name: 'insert query',
+	    text: 'INSERT INTO velocity_boundary (id, name, field, pipelinefn, tags, userid, kafka_msg)) values ($1, $2, $3, $4)',
+	    values: [id, name, field, pipelinefn, tags, userid, line]
+	});
+	// console.log('inserted.'); 
+    }
+}); 
+
 
 //////////////////////////////////
 // insert data into the database./
 //////////////////////////////////
-query = client.query({
-    name: 'insert query',
-    text: 'INSERT INTO velocity_boundary (id, name, field, userid) values ($1, $2, $3, $4)',
-    values: ['fid-0022123233', 'test-fid', 'ST LOUIS', 'simon']
-});
+// query = client.query({
+//     name: 'insert query',
+//     text: 'INSERT INTO velocity_boundary (id, name, field, userid) values ($1, $2, $3, $4)',
+//     values: ['fid-0022123233', 'test-fid', 'ST LOUIS', 'simon']
+// });
 
 query.on('end', function () {
     client.end(); 
