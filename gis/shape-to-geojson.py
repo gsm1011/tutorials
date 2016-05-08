73cab1ede6f7e2f743ba5e7fedb36e6918b3d3b2
Shumin <gsmsteve@gmail.com>
2016-05-08 16:23:40 -0500

    Fixed some issue with the shape-to-geojson.

--- gis/shape-to-geojson.py
+++ gis/shape-to-geojson.py
@@ -2,14 +2,12 @@ import shapefile
 
 reader = shapefile.Reader("./test.shp")
 fields = reader.fields[1:]
-field_names = [fields[0] for field in fields]
+field_names = [field[0] for field in fields]
 
 buffer = []
 
 for sr in reader.shapeRecords():
-    # print field_names
-    print zip(sr.record, field_names)
-    atr = dict(zip(sr.record, field_names))
+    atr = dict(zip(field_names, sr.record))
     geom = sr.shape.__geo_interface__
     buffer.append(dict(type = "Feature", geometry = geom, properties = atr))
 
