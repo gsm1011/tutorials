4065aaac9e93fe9d2b052ef79dc4fb5eb9e0727a
Shumin Guo <gsmsteve@gmail.com>
2016-05-12 17:50:13 -0500

    Updated shape to geojson script.

--- gis/shape-to-geojson.py
+++ gis/shape-to-geojson.py
@@ -1,17 +1,30 @@
 import shapefile
+import sys
 
-reader = shapefile.Reader("./test.shp")
+filename = sys.argv[1]
+
+if filename == '':
+    print "Usage: python shape-to-geojson.py <shapefile>"
+    exit (1)
+
+basename = filename[0:filename.index('.')]
+print 'basename is: ', basename
+
+reader = shapefile.Reader(filename)
 fields = reader.fields[1:]
 field_names = [field[0] for field in fields]
-
 buffer = []
 
 for sr in reader.shapeRecords():
+    # print field_names
+    # print zip(sr.record, field_names)
     atr = dict(zip(field_names, sr.record))
     geom = sr.shape.__geo_interface__
-    buffer.append(dict(type = "Feature", geometry = geom, properties = atr))
+    buffer.append(dict(type = "Feature", id = basename, geometry = geom, properties = atr))
+
+crs = { "properties": {"name": "EPSG:4326" }, "type": "name" }
 
 from json import dumps
-geojson = open("shape-to-geojson.json", "w")
-geojson.write(dumps({"type": "FeatureCollection", "features": buffer}, indent = 2) + "\n")
+geojson = open(basename + ".json", "w")
+geojson.write(dumps({"crs": crs, "type": "FeatureCollection", "features": buffer}, indent = 2) + "\n")
 geojson.close()
