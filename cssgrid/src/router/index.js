8e3cb075ed973338c0a60f026454bb1b597e6449
Shumin Guo <gsmsteve@gmail.com>
2018-07-20 12:01:24 -0400

    Added routes for new components.

--- cssgrid/src/router/index.js
+++ cssgrid/src/router/index.js
@@ -1,15 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Grid1 from '@/components/Grid1'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      name: 'Login',
+      component: () => import('@/components/Login')
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'GridBasics',
+      component: () => import('@/components/GridBasics')
+    },
+    {
+      path: '/grid1',
+      name: 'Grid1',
+      component: Grid1
+    },
+    {
+      path: '/grid2',
+      name: 'Grid2',
+      component: () => import('@/components/Grid2')
+
+    },
+    {
+      path: '/grid3',
+      name: 'Grid3',
+      component: () => import('@/components/Grid3')
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: () => import('@/components/About')
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: () => import('@/components/404')
     }
   ]
 })
