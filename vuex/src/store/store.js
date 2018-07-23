76593dfd879ed7afd2788ba1c6417356ae54107c
Shumin Guo <gsmsteve@gmail.com>
2018-07-23 16:56:44 -0400

    Vuex - Step 3: change data using mutations.

--- vuex/src/store/store.js
+++ vuex/src/store/store.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
+  strict: true,
   state: {
     products: [
       { name: 'Banana Skin', price: 20 },
@@ -11,5 +12,23 @@ export const store = new Vuex.Store({
       { name: 'Green shells', price: 60 },
       { name: 'Red shells', price: 80 }
     ]
+  },
+  getters: {
+    saleProducts: state => {
+      var saleProducts = state.products.map(product => {
+        return {
+          name: '**' + product.name + '**',
+          price: product.price / 2
+        }
+      });
+      return saleProducts;
+    }
+  },
+  mutations: {
+    reducePrice: state => {
+      state.products.forEach(product => {
+        product.price -= 1;
+      })
+    }
   }
-})
+});
